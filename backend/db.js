8f7efd03ebb9c0936c719a21bbf1eeaa83f747d4
Priyanshu Sharma <139767064+priyanshushrma01@users.noreply.github.com>
2024-04-29 23:33:25 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://admin:YH5KM2DgtgD8q0Hd@cluster0.qjeoqfv.mongodb.net/todos");
+mongoose.connect("mongodb+srv://********/todos");
 
 const todoSchema = mongoose.Schema({
     title:String,
